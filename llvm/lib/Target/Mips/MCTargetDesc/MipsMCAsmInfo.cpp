25443f91c1bc1325008d47c63065f33b4c464672
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2023-03-20 23:57:11 +0800

    Add a nanoMIPS parser variant to allow brackets in mnemonics
    Change-Id: Ib61ddb42057eaa631b5c4aa219ca76b291835702

--- llvm/lib/Target/Mips/MCTargetDesc/MipsMCAsmInfo.cpp
+++ llvm/lib/Target/Mips/MCTargetDesc/MipsMCAsmInfo.cpp
@@ -54,6 +54,8 @@ MipsELFMCAsmInfo::MipsELFMCAsmInfo(const Triple &TheTriple,
     UseIntegratedAssembler = false;
   if (ABI.IsP32())
     HasLEB128Directives = false;
+  if (TheTriple.isNanoMips())
+    AssemblerDialect = 1;
 }
 
 void MipsCOFFMCAsmInfo::anchor() {}

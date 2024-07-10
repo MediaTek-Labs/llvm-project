61da85de7d0129726c72bdc72ddb10856a1c7f13
Yishai Admanit <yishai.admanit@mediatek.com>
2024-07-10 22:56:28 +0200

    Fixup! add callee address to kCFI recover report
    Change-Id: Id00d2b3f8cc3b06d9ffceda5d062f79be29ae2ef

--- llvm/lib/Transforms/Instrumentation/KCFI.cpp
+++ llvm/lib/Transforms/Instrumentation/KCFI.cpp
@@ -121,16 +121,16 @@ PreservedAnalyses KCFIPass::run(Function &F, FunctionAnalysisManager &AM) {
       Builder.CreateUnreachable();
       TrapCall->getParent()->getTerminator()->eraseFromParent();
     } else {
-      FunctionCallee WarningFn;
+      FunctionCallee HandlerFn;
       Type *OpType = PointerType::get(Type::getInt8Ty(Builder.getContext()), 0);
       Value *Data = emitDebugLocData(DebugLoc, M, Builder);
-      WarningFn = M.getOrInsertFunction(
+      HandlerFn = M.getOrInsertFunction(
           "__ubsan_handle_kcfi", Builder.getVoidTy(), Data->getType(), OpType);
-      CallInst *Call = Builder.CreateCall(
-          WarningFn,
-          {Data, Builder.CreateBitCast(CI->getCalledOperand(), OpType)});
-      Call->setDebugLoc(DebugLoc);
-      Call->setCannotMerge();
+      CallInst *HandlerCall = Builder.CreateCall(
+          HandlerFn,
+          {Data, Builder.CreateBitCast(Call->getCalledOperand(), OpType)});
+      HandlerCall->setDebugLoc(DebugLoc);
+      HandlerCall->setCannotMerge();
     }
     ++NumKCFIChecks;
   }

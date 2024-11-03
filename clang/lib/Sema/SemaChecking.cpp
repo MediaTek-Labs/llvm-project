b573d358ec2fdc52485ef24d78e563169227c83a
Yishai Admanit <yishai.admanit@mediatek.com>
2024-11-03 02:05:19 +0100

    support ABSQ_S.[PH|QB|W], PRECE[Q|U|QU].*
    Change-Id: I06a6ab31d7f8918b351d90b1cc529dea0b847146

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -3855,6 +3855,9 @@ bool Sema::CheckNanomipsBuiltinCpu(unsigned BuiltinID, CallExpr *TheCall) {
   switch (BuiltinID) {
   default:
     return false;
+  case Mips::BI__builtin_mips_absq_s_ph:
+  case Mips::BI__builtin_mips_absq_s_qb:
+  case Mips::BI__builtin_mips_absq_s_w:
   case Mips::BI__builtin_mips_addq_ph:
   case Mips::BI__builtin_mips_addq_s_ph:
   case Mips::BI__builtin_mips_addqh_ph:
@@ -3895,6 +3898,16 @@ bool Sema::CheckNanomipsBuiltinCpu(unsigned BuiltinID, CallExpr *TheCall) {
   case Mips::BI__builtin_mips_packrl_ph:
   case Mips::BI__builtin_mips_pick_ph:
   case Mips::BI__builtin_mips_pick_qb:
+  case Mips::BI__builtin_mips_preceq_w_phl:
+  case Mips::BI__builtin_mips_preceq_w_phr:
+  case Mips::BI__builtin_mips_precequ_ph_qbl:
+  case Mips::BI__builtin_mips_precequ_ph_qbla:
+  case Mips::BI__builtin_mips_precequ_ph_qbr:
+  case Mips::BI__builtin_mips_precequ_ph_qbra:
+  case Mips::BI__builtin_mips_preceu_ph_qbl:
+  case Mips::BI__builtin_mips_preceu_ph_qbla:
+  case Mips::BI__builtin_mips_preceu_ph_qbr:
+  case Mips::BI__builtin_mips_preceu_ph_qbra:
   case Mips::BI__builtin_mips_precrq_ph_w:
   case Mips::BI__builtin_mips_rddsp:
   case Mips::BI__builtin_mips_shilo:

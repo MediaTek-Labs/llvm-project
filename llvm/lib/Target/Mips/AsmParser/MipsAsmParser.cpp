4a16ead0ca46876ea275b1536721bf5e6021bfdb
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2025-03-18 04:03:17 +0800

    [Bug141 2/3] Fix error-handling for ADDIU[GP.B] and ADDIU[GP.W]
    Change-Id: Ifb8c9cb037bf14e28c7345d5123c2c5b6458a6d1

--- llvm/lib/Target/Mips/AsmParser/MipsAsmParser.cpp
+++ llvm/lib/Target/Mips/AsmParser/MipsAsmParser.cpp
@@ -7113,6 +7113,10 @@ bool MipsAsmParser::MatchAndEmitInstruction(SMLoc IDLoc, unsigned &Opcode,
     }
   case Match_SymGPRel:
     return Error(IDLoc, "expected 32-bit signed/unsigned GP-relative offset");
+  case Match_SymGPRel18:
+    return Error(IDLoc, "expected 18-bit unsigned GP-relative offset");
+  case Match_SymGPRel19s2:
+    return Error(IDLoc, "expected word-aligned 21-bit unsigned GP-relative offset");
   case Match_RequiresBaseSP:
     return Error(IDLoc, "expected $sp as base register");
   case Match_RequiresBaseGP:
--- llvm/test/MC/Mips/nanomips/invalid.s
+++ llvm/test/MC/Mips/nanomips/invalid.s
@@ -9,3 +9,13 @@ foo:
         sc  $a2, -1($s4) # CHECK: error: invalid operand for instruction
 	addiu.b32 $a4, $gp, -2147483649 # CHECK: [[@LINE]]:{{[0-9]+}}: error: expected 32-bit signed/unsigned GP-relative offset
 	addiu.b32 $a4, $gp, 4294967296 # CHECK: [[@LINE]]:{{[0-9]+}}: error: expected 32-bit signed/unsigned GP-relative offset
+	addiu.b $a0, $gp, 262144	# > 18 bits
+	# CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected 18-bit unsigned GP-relative offset
+	addiu[gp.b] $a0, $gp, -262143	# -ve
+	# CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected 18-bit unsigned GP-relative offset
+	addiu.w $a0, $gp, 524286	# > 21 bits
+	# CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected word-aligned 21-bit unsigned GP-relative offset
+	addiu.w $a0, $gp, -524284	# -ve
+	# CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected word-aligned 21-bit unsigned GP-relative offset
+	addiu[gp.w] $a0, $gp, 524283	# mis-aligned
+	# CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected word-aligned 21-bit unsigned GP-relative offset

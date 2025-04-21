09c3ba1645d0dbfa01560494f837db219f323daf
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2025-04-21 12:32:01 +0800

    [Bug 159] Accept GPR notation for COP0 registers in MFTR/MTTR
    This is used in DV testing and supported for compatibility with
    GNU assembler.
    
    Change-Id: I755d29a3843a879fc3a77be3354dbd66efafd788

--- llvm/test/MC/Mips/mt/valid-nanomips.s
+++ llvm/test/MC/Mips/mt/valid-nanomips.s
@@ -51,3 +51,7 @@
   mttr $a0, $31, 1, 3, 0    # CHECK:  mttr  $a0, $31, 1, 3, 0    # encoding: [0x9f,0x20,0x70,0x1e]
   mttr $a0, $14, 1, 4, 0    # CHECK:  mttr  $a0, $14, 1, 4, 0    # encoding: [0x8e,0x20,0x70,0x26]
   mttr $a0, $15, 1, 5, 0    # CHECK:  mttr  $a0, $15, 1, 5, 0    # encoding: [0x8f,0x20,0x70,0x2e]
+  mftr $a0, $a1, 0, 2, 0    # CHECK:  mftr  $a0, $a1, 0, 2, 0     # encoding: [0x85,0x20,0x30,0x12]
+                            # DISAS: {{.*}}: 85 20 30 12    mftr $a0, $5, 0, 2, 0
+  mttr $a0, $a1, 0, 2, 0    # CHECK:  mttr  $a0, $a1, 0, 2, 0     # encoding: [0x85,0x20,0x70,0x12]
+                            # DISAS: {{.*}}: 85 20 70 12    mttr $a0, $5, 0, 2, 0

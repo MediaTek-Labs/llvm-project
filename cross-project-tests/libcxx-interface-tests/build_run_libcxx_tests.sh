37d5e3a60d1c151b060b67204ca6ebc24892c091
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2025-05-16 22:10:02 +0800

    Add libcxx stubs for lock/unlock
    Empty stubs satisfy the link requirement which is sufficient to run
    libcxx test suite without too many link failures.
    
    Change-Id: I5bb2dbcba54bdd797004bef8e700845743e6bb0b

--- cross-project-tests/libcxx-interface-tests/build_run_libcxx_tests.sh
+++ cross-project-tests/libcxx-interface-tests/build_run_libcxx_tests.sh
@@ -7,42 +7,21 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 
-build_run_gettimeofday_dependent_test() {
-
-        local test_name="$1"
-
-        # Make test object file
-        $toolchain/bin/nanomips-elf-clang++ -c $SRC_DIR/$test_name.cpp -o $test_name.o -march=i7200 -Tuhi32.ld -fno-exceptions -lm -Wl,--defsym,__memory_size=256M
-        if [ $? -ne 0 ]; then
-                echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: $test_name <<<<<<<<<<<<<<<<<<<<\n"
-                return
-        fi
-
-        # Make libstubs object file
-        $toolchain/bin/nanomips-elf-clang -c $SRC_DIR/../../libcxx/test/libstubs.c -o libstubs.o -march=i7200 -Tuhi32.ld -fno-exceptions -lm  -Wl,--defsym,__memory_size=256M
-        if [ $? -ne 0 ]; then
-                echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: libstubs <<<<<<<<<<<<<<<<<<<<\n"
-                return
-        fi
-
-        # Link test.o and libstubs.o
-        $toolchain/bin/nanomips-elf-clang++ $test_name.o libstubs.o -o $test_name.elf -march=i7200 -Tuhi32.ld -fno-exceptions -lm  -Wl,--defsym,__memory_size=256M
-        if [ $? -ne 0 ]; then
-                echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: $test_name <<<<<<<<<<<<<<<<<<<<\n"
-                return
-        fi
-
-        # Run linked test
-        $toolchain/bin/qemu-system-nanomips -cpu I7200 -m 256 -semihosting -nographic -kernel $test_name.elf &> $test.out
-        if [ $? -ne 0 ]; then
-                echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: $test_name <<<<<<<<<<<<<<<<<<<<\n"
-                return
-        fi
-
-	echo -e ">>>>>>>>>>>>>>>>>>>> PASSED: $test_name <<<<<<<<<<<<<<<<<<<<\n"
-}
-
 toolchain=$1;
+
+# Make libstubs object file
+$toolchain/bin/nanomips-elf-clang -c $SRC_DIR/../../libcxx/test/support/libstubs.c -o libstubs.o -march=i7200 -Tuhi32.ld -lm  -Wl,--defsym,__memory_size=256M
+if [ $? -ne 0 ]; then
+	echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: libstubs <<<<<<<<<<<<<<<<<<<<\n"
+	return
+fi
+$toolchain/bin/nanomips-elf-clang++ -c $SRC_DIR/../../libcxx/test/support/libcxx_stubs.cpp -o libcxx_stubs.o -march=i7200 -Tuhi32.ld -lm  -Wl,--defsym,__memory_size=256M
+if [ $? -ne 0 ]; then
+	echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: libstubs.cpp <<<<<<<<<<<<<<<<<<<<\n"
+	return
+fi
+$toolchain/bin/nanomips-elf-ar cru libstubs.a libstubs.o libcxx_stubs.o
+
 
 tests=(algorithm array chrono climits cstdbool ctype fenv inttypes limits map
        math memory queue random set stdarg stdbool stdint stdio stdlib
@@ -54,35 +33,30 @@ for test in ${tests[@]}; do
 
 	echo ">>>>>>>>>>>>>>>>>>>> $test <<<<<<<<<<<<<<<<<<<<"
 
-	if [[ $test == "chrono" || $test == "time" ]]; then
-		build_run_gettimeofday_dependent_test $test
-	else
-		# Compile test
-		$toolchain/bin/nanomips-elf-clang++ $SRC_DIR/$test.cpp -o $test.elf -march=i7200 -Tuhi32.ld -fno-exceptions -lm -Wl,--defsym,__memory_size=256M
+	# Compile test
+	$toolchain/bin/nanomips-elf-clang++ $SRC_DIR/$test.cpp -o $test.elf -march=i7200 -Tuhi32.ld -Wl,--whole-archive -lstubs -Wl,--no-whole-archive -lm -Wl,--defsym,__memory_size=256M
 
-		if [ $? -ne 0 ]; then
-			echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: $test <<<<<<<<<<<<<<<<<<<<\n"
+	if [ $? -ne 0 ]; then
+		echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: $test <<<<<<<<<<<<<<<<<<<<\n"
+		continue
+	fi
+
+	if [[ $test == "stdio" ]]; then
+		touch file_to_be_removed_by_std::remove.txt
+		touch file_to_be_renamed_by_std::rename.txt
+	fi
+
+	# Run test
+	$toolchain/bin/qemu-system-nanomips -cpu I7200 -m 256 -semihosting -nographic -kernel $test.elf &> $test.out
+
+	if [ $? -ne 0 ]; then
+		if [[ $test == "stdlib_exit_1" || $test == "stdlib_exit_3" || $test == "stdlib_exit_5" ]]; then
+		# The exit code of these tests is supposed to be non-zero
+			echo -e ">>>>>>>>>>>>>>>>>>>> PASSED: $test <<<<<<<<<<<<<<<<<<<<\n"
 		else
-			if [[ $test == "stdio" ]]; then
-				touch file_to_be_removed_by_std::remove.txt
-				touch file_to_be_renamed_by_std::rename.txt
-			fi
-
-			# Run test
-			$toolchain/bin/qemu-system-nanomips -cpu I7200 -m 256 -semihosting -nographic -kernel $test.elf &> $test.out
-
-			if [ $? -ne 0 ]; then
-				if [[ $test == "stdlib_exit_1" || $test == "stdlib_exit_3" || $test == "stdlib_exit_5" ]]; then
-					# The exit code of these tests is supposed to be non-zero
-					echo -e ">>>>>>>>>>>>>>>>>>>> PASSED: $test <<<<<<<<<<<<<<<<<<<<\n"
-				else
-					echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: $test <<<<<<<<<<<<<<<<<<<<\n"
-				fi
-			else
-				echo -e ">>>>>>>>>>>>>>>>>>>> PASSED: $test <<<<<<<<<<<<<<<<<<<<\n"
-			fi
+			echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: $test <<<<<<<<<<<<<<<<<<<<\n"
 		fi
+	else
+		echo -e ">>>>>>>>>>>>>>>>>>>> PASSED: $test <<<<<<<<<<<<<<<<<<<<\n"
 	fi
 done
-
-

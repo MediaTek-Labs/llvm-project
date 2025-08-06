7c66bb4e35ec4bcfab75247f193c81b75d688b67
Yishai Admanit <yishai.admanit@mediatek.com>
2025-08-06 20:45:04 +0200

    Swap operands in PREPEND to EXTW conversion
    Change-Id: Icbbf30318bb054a4269170e27ecaaa54eb1150a2

--- llvm/test/MC/Mips/nanomips/dsp-aliases.s
+++ llvm/test/MC/Mips/nanomips/dsp-aliases.s
@@ -11,11 +11,11 @@
 	balign	$a0, $a1, 3
 # CHECK:{{.*}}move $a0, $a0
         prepend $a0, $a1, 0
-# CHECK:{{.*}}extw $a0, $a1,{{.*}}0x1
+# CHECK:{{.*}}extw $a0, $a0, $a1,{{.*}}0x1
         prepend $a0, $a1, 1
-# CHECK:{{.*}}extw $a0, $a1,{{.*}}0x2
+# CHECK:{{.*}}extw $a0, $a0, $a1,{{.*}}0x2
         prepend $a0, $a1, 2
-# CHECK:{{.*}}extw $a0, $a1,{{.*}}0x3
+# CHECK:{{.*}}extw $a0, $a0, $a1,{{.*}}0x3
         prepend $a0, $a1, 3
-# CHECK:{{.*}}extw $a0, $a1,{{.*}}0x1f
+# CHECK:{{.*}}extw $a0, $a0, $a1,{{.*}}0x1f
         prepend $a0, $a1, 31

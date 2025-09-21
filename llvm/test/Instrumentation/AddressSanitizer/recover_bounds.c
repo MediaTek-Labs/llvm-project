2bd7af889775484e68d7029be76db1c38b36cbac
Yishai Admanit <yishai.admanit@mediatek.com>
2025-09-21 22:44:17 +0200

    Explicit parameter in call of local bounds sanitizer handler The parameter type is struct { char *Filename; unsigned Line; unsigned Col; };
    Change-Id: I9207dec7489b4b94a60457d069efdcbce69ade33

--- llvm/test/Instrumentation/AddressSanitizer/recover_bounds.c
+++ llvm/test/Instrumentation/AddressSanitizer/recover_bounds.c
@@ -9,42 +9,61 @@
 // RUN:   -fsanitize-trap=bounds  -emit-llvm -o - | \
 // RUN:   FileCheck %s --check-prefix TRAP
 
-// RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds \
-// RUN:   -fsanitize-recover=bounds -g -emit-llvm -o - | \
-// RUN:   FileCheck %s --check-prefixes HANDLER,RECOVER --implicit-check-not DEFAULT
+// RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds -fsanitize-trap \
+// RUN:   -mllvm -explicit-param-bounds-checking-handler \
+// RUN:   -fno-sanitize-trap=bounds -fsanitize-recover=bounds -g -emit-llvm -o - | \
+// RUN:   FileCheck %s --check-prefix RECOVER-EXP
+
+// RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds -fno-sanitize-trap \
+// RUN:   -mllvm -explicit-param-bounds-checking-handler \
+// RUN:   -g -emit-llvm -o - | \
+// RUN:   FileCheck %s --check-prefix ABORT-EXP
+
+// RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds -fsanitize-trap \
+// RUN:   -mllvm -explicit-param-bounds-checking-handler \
+// RUN:   -fno-sanitize-trap=bounds -g -emit-llvm -o - | \
+// RUN:   FileCheck %s --check-prefix ABORT-EXP
 
 // RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds -fsanitize-trap \
 // RUN:   -fno-sanitize-trap=bounds -fsanitize-recover=bounds -g -emit-llvm -o - | \
-// RUN:   FileCheck %s --check-prefixes HANDLER,RECOVER --implicit-check-not DEFAULT
+// RUN:   FileCheck %s --check-prefix RECOVER-IMP
 
 // RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds -fno-sanitize-trap \
 // RUN:   -g -emit-llvm -o - | \
-// RUN:   FileCheck %s --check-prefixes HANDLER,DEFAULT
+// RUN:   FileCheck %s --check-prefix ABORT-IMP
 
 // RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds -fsanitize-trap \
 // RUN:   -fno-sanitize-trap=bounds -g -emit-llvm -o - | \
-// RUN:   FileCheck %s --check-prefixes HANDLER,DEFAULT
+// RUN:   FileCheck %s --check-prefix ABORT-IMP
 
 // RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds \
+// RUN:   -mllvm -explicit-param-bounds-checking-handler \
 // RUN:   -fsanitize-trap=bounds -g -emit-llvm -o - | \
 // RUN:   FileCheck %s --check-prefix TRAP
 
-// RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds \
-// RUN:   -fno-sanitize-recover -g -emit-llvm -o - | \
-// RUN:   FileCheck %s --check-prefixes HANDLER,DEFAULT
-
-// RUN: clang %s -S -Wno-array-bounds -fsanitize=bounds \
-// RUN:   -fsanitize-recover -fno-sanitize-recover=bounds -g -emit-llvm -o - | \
-// RUN:   FileCheck %s --check-prefixes HANDLER,DEFAULT
-
-// TRAP: call void @llvm.trap()
+// TRAP: call void @llvm.ubsantrap
 // TRAP-NEXT: unreachable
-// HANDLER: [[VAR1:@[0-9]+]] = private constant {{.*}}c"{{.*}}/recover_bounds.c\00"
-// HANDLER: [[VAR2:@[0-9]+]] = private constant { ptr, i32, i32 } { ptr [[VAR1]], i32 100, i32 10 }
-// RECOVER: call void @__ubsan_handle_out_of_bounds_minimal(ptr [[VAR2]])
-// RECOVER-NEXT: br label
-// DEFAULT: call void @__ubsan_handle_out_of_bounds_minimal_abort(ptr [[VAR2]])
-// DEFAULT-NEXT: unreachable
+
+// RECOVER-EXP: [[VAR:@[0-9]+]] = private constant {{.*}}c"{{.*}}/recover_bounds.c\00"
+// RECOVER-EXP: [[VAR2:@[0-9]+]] = private constant { ptr, i32, i32 } { ptr [[VAR]], i32 100, i32 10 }
+// RECOVER-EXP-LABEL: trap:
+// RECOVER-EXP: call void @__ubsan_handle_local_out_of_bounds_explicit_param(ptr [[VAR2]])
+// RECOVER-EXP-NEXT: br label
+
+// RECOVER-IMP-LABEL: trap:
+// RECOVER-IMP: call void @__ubsan_handle_local_out_of_bounds()
+// RECOVER-IMP-NEXT: br label
+
+// ABORT-EXP: [[VAR:@[0-9]+]] = private constant {{.*}}c"{{.*}}/recover_bounds.c\00"
+// ABORT-EXP: [[VAR2:@[0-9]+]] = private constant { ptr, i32, i32 } { ptr [[VAR]], i32 100, i32 10 }
+// ABORT-EXP-LABEL: trap:
+// ABORT-EXP: call void @__ubsan_handle_local_out_of_bounds_abort_explicit_param(ptr [[VAR2]])
+// ABORT-EXP-NEXT: unreachable
+
+// ABORT-IMP-LABEL: trap:
+// ABORT-IMP: call void @__ubsan_handle_local_out_of_bounds_abort()
+// ABORT-IMP-NEXT: unreachable
+
 int arr[2];
 int foo(int n) {
 #line 100

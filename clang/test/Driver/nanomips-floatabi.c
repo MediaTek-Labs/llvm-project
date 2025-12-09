f4565cb67d94d417107bacb58519070895eabd8b
Barbara Mitic <ot_barbara.mitic@mediatek.com>
2025-12-09 10:57:32 +0100

    Follow up to bug165: tiny change in nanomips-floatabi.c test
    The test runs a clang driver command that intentionally fails.
    Adding not tells lit that the non-zero exit is expected, so FileCheck
    can run and verify the error message.
    Without not, lit stops the test at the failed command.
    
    Change-Id: I6875050f9319bbb279a80f988ad9103e768ee59d

--- clang/test/Driver/nanomips-floatabi.c
+++ clang/test/Driver/nanomips-floatabi.c
@@ -1,6 +1,6 @@
 // Test that target feature ite is implemented and available correctly
 
-// RUN: %clang -### -target nanomips-elf -mhard-float %s 2>&1 | FileCheck %s --check-prefix=HARDFLOAT
+// RUN: not %clang -### -target nanomips-elf -mhard-float %s 2>&1 | FileCheck %s --check-prefix=HARDFLOAT
 
 // RUN: %clang -### -target nanomips-elf -msoft-float %s 2>&1 | FileCheck %s
 

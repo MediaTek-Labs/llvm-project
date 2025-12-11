804eaf026668cb1b8633f0c33c873b1efc559db3
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2025-12-11 21:57:01 +0800

    Update test scripts for libcxx_interface
    * Provide path to qemu as argument, no longer in toolchain path
    * Use llvm-ar instead of GNU
    
    Change-Id: I7ba92d4670ae4d88930f5fe9bc4ee37392102187

--- cross-project-tests/libcxx-interface-tests/build_run_libcxx_tests.sh
+++ cross-project-tests/libcxx-interface-tests/build_run_libcxx_tests.sh
@@ -3,11 +3,12 @@
 SRC_DIR=`dirname $0`
 
 if [ $# -lt 1 ]; then
-	echo "Usage: ./build_run_libcxx_tests <path_to_elf_toolchain>"
+	echo "Usage: ./build_run_libcxx_tests <path_to_elf_toolchain> <path_to_qemu>"
 	exit 1
 fi
 
 toolchain=$1;
+sim=$2
 
 # Make libstubs object file
 $toolchain/bin/nanomips-elf-clang -c $SRC_DIR/../../libcxx/test/support/libstubs.c -o libstubs.o -march=i7200 -Tuhi32.ld -lm  -Wl,--defsym,__memory_size=256M
@@ -20,7 +21,7 @@ if [ $? -ne 0 ]; then
 	echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: libstubs.cpp <<<<<<<<<<<<<<<<<<<<\n"
 	return
 fi
-$toolchain/bin/nanomips-elf-ar cru libstubs.a libstubs.o libcxx_stubs.o
+$toolchain/bin/nanomips-elf-llvm-ar cru libstubs.a libstubs.o libcxx_stubs.o
 
 
 tests=(algorithm array chrono climits cstdbool ctype fenv inttypes limits map
@@ -34,7 +35,7 @@ for test in ${tests[@]}; do
 	echo ">>>>>>>>>>>>>>>>>>>> $test <<<<<<<<<<<<<<<<<<<<"
 
 	# Compile test
-	$toolchain/bin/nanomips-elf-clang++ $SRC_DIR/$test.cpp -o $test.elf -march=i7200 -Tuhi32.ld -Wl,--whole-archive -lstubs -Wl,--no-whole-archive -lm -Wl,--defsym,__memory_size=256M
+	$toolchain/bin/nanomips-elf-clang++ $SRC_DIR/$test.cpp -o $test.elf -march=i7200 -Tuhi32.ld -L. -Wl,--whole-archive -lstubs -Wl,--no-whole-archive -lm -Wl,--defsym,__memory_size=256M
 
 	if [ $? -ne 0 ]; then
 		echo -e ">>>>>>>>>>>>>>>>>>>> FAILED: $test <<<<<<<<<<<<<<<<<<<<\n"
@@ -47,7 +48,7 @@ for test in ${tests[@]}; do
 	fi
 
 	# Run test
-	$toolchain/bin/qemu-system-nanomips -cpu I7200 -m 256 -semihosting -nographic -kernel $test.elf &> $test.out
+	$sim -cpu I7200 -m 256 -semihosting -nographic -kernel $test.elf &> $test.out
 
 	if [ $? -ne 0 ]; then
 		if [[ $test == "stdlib_exit_1" || $test == "stdlib_exit_3" || $test == "stdlib_exit_5" ]]; then

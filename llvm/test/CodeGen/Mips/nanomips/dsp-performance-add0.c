c15c9a83d1a90a46c70a37d696704526e3f3a977
Yishai Admanit <yishai.admanit@mediatek.com>
2025-12-19 16:47:13 +0100

    Fixup! [Bug 255] - adapt test to restoration of side effect of mips builtins
    Change-Id: Iea68d482ab2228ada17a9f71a1aab28ef6a8cc8c

--- llvm/test/CodeGen/Mips/nanomips/dsp-performance-add0.c
+++ llvm/test/CodeGen/Mips/nanomips/dsp-performance-add0.c
@@ -1,30 +1,40 @@
 // RUN: clang %s -O0 --target=nanomips -S -mdsp -o - | FileCheck %s --check-prefixes NOOPT,ALL
 // RUN: clang %s -O2 --target=nanomips -S -mdsp -o - | FileCheck %s --check-prefixes OPT,ALL
 
+// Under current builtin model addq/subq have side effects
+// thus they are not optimized out
+// but their results are not being used
+// Under -O0 flag their result is legalized by seh
+// Under optimization flag their result is ignored
+
 // ALL-LABEL: addq_zero1_test
 short addq_zero1_test(short a) {
-// NOOPT: addq_s.w
-// OPT-NOT: addq_s.w
+// ALL: addq_s.w
+// NOOPT: seh
+// OPT-NOT: seh
   return __builtin_mips_addq_s_w(0, a);
 }
 
 // ALL-LABEL: subq_zero1_test
 short subq_zero1_test(short a) {
-// NOOPT: subq_s.w
-// OPT-NOT: subq_s.w
+// ALL: subq_s.w
+// NOOPT: seh
+// OPT-NOT: seh
   return __builtin_mips_subq_s_w(0, a);
 }
 
 // ALL-LABEL: addq_zero2_test
 short addq_zero2_test(short a) {
-// NOOPT: addq_s.w
-// OPT-NOT: addq_s.w
+// ALL: addq_s.w
+// NOOPT: seh
+// OPT-NOT: seh
   return __builtin_mips_addq_s_w(a, 0);
 }
 
 // ALL-LABEL: subq_zero2_test
 short subq_zero2_test(short a) {
-// NOOPT: subq_s.w
-// OPT-NOT: subq_s.w
+// ALL: subq_s.w
+// NOOPT: seh
+// OPT-NOT: seh
   return __builtin_mips_subq_s_w(a, 0);
 }
